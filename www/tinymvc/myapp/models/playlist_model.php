cf417f06602a1b76eb2b130f051bea01d751a15a
limbergivo <44265492+limbergivo@users.noreply.github.com>
2019-02-09 16:20:37 +0100

    added model playlist

--- www/tinymvc/myapp/models/playlist_model.php
+++ www/tinymvc/myapp/models/playlist_model.php
@@ -1,33 +1,88 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: error
- * Date: 9/02/2019
- * Time: 15:50
- */
 
 class Playlist_Model extends TinyMVC_Model
 {
-    private $songId, $userId, $artistId, $songTitleId, $urlId;
+private $songId, $userId, $artistId, $songTitleId, $urlId;
+
+/**
+* @return mixed
+*/
+public function getSongId()
+{
+return $this->songId;
+}
+
+/**
+* @param mixed $songId
+*/
+public function setSongId($songId)
+{
+$this->songId = $songId;
+}
 
     /**
      * @return mixed
      */
-    public function getSongId()
+    public function getUserId()
     {
-        return $this->songId;
+        return $this->userId;
     }
 
     /**
-     * @param mixed $songId
+     * @param mixed $userId
      */
-    public function setSongId($songId)
+    public function setUserId($userId)
     {
-        $this->songId = $songId;
+        $this->userId = $userId;
     }
-    public function getSongIdFromDB()
-    {
 
+    /**
+     * @return mixed
+     */
+    public function getArtistId()
+    {
+        return $this->artistId;
     }
+
+    /**
+     * @param mixed $artistId
+     */
+    public function setArtistId($artistId)
+    {
+        $this->artistId = $artistId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSongTitleId()
+    {
+        return $this->songTitleId;
+    }
+
+    /**
+     * @param mixed $songTitleId
+     */
+    public function setSongTitleId($songTitleId)
+    {
+        $this->songTitleId = $songTitleId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrlId()
+    {
+        return $this->urlId;
+    }
+
+    /**
+     * @param mixed $urlId
+     */
+    public function setUrlId($urlId)
+    {
+        $this->urlId = $urlId;
+    }
+
 
 }
