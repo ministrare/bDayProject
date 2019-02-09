68f1cb5f4bbb934bb6757fd4ed7db03dfedbd838
Lesley Forn <ministrare@hotmail.com>
2019-02-09 12:55:44 +0100

    removed setter page_model

---
+++ www/tinymvc/myapp/models/functions_model.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minis
+ * Date: 9/02/2019
+ * Time: 12:45
+ */
+
+class Functions_Model extends TinyMVC_Model
+{
+    private $functions;
+
+
+    public function __construct($poolname = null)
+    {
+        parent::__construct($poolname);
+    }
+}
