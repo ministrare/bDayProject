a4eaa0c8159e9479bd4f4c842732e71e3d8ae32f
elmahdi <elmahdi.alajdielidrissi@gmail.com>
2019-02-09 14:16:00 +0100

    basic form in index and login view

--- www/tinymvc/myapp/views/index_view.php
+++ www/tinymvc/myapp/views/index_view.php
@@ -1,3 +1,9 @@
-halllooo index
-
-
+<div class="row container">
+    <div class="col-6"></div>
+<form action="" class="form-signin col-6 justify-content-md-center" method="post">
+    <h1 class="h3 mb-3 font-weight-normal">Vul hier je gegevens in</h1>
+    <input type="email" name="email" class="form-control" placeholder="Email adres">
+    <input type="text" name="username" class="form-control" placeholder="Gebruikersnaam">
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Ga verder</button>
+</form>
+</div>
--- www/tinymvc/myapp/views/login_view.php
+++ www/tinymvc/myapp/views/login_view.php
@@ -1,1 +1,20 @@
-check
+<div class="row container">
+    <div class="col-6"></div>
+    <form action="" class="col-6 justify-content-md-center" method="post">
+        <h1 class="h3 mb-3 font-weight-normal">Vul je meest favoriete nummers in</h1>
+        <input type="text" name="artist1" class="form-control" placeholder="Artiest naam">
+        <input type="text" name="song1" class="form-control" placeholder="Titel van het lied">
+        <input type="text" name="url1" class="form-control" placeholder="Youtube link van het lied">
+        <br>
+
+        <input type="text" name="artist2" class="form-control" placeholder="Artiest naam">
+        <input type="text" name="song2" class="form-control" placeholder="Titel van het lied">
+        <input type="text" name="url2" class="form-control" placeholder="Youtube link van het lied">
+        <br>
+
+        <input type="text" name="artist3" class="form-control" placeholder="Artiest naam">
+        <input type="text" name="song3" class="form-control" placeholder="Titel van het lied">
+        <input type="text" name="url3" class="form-control" placeholder="Youtube link van het lied">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Verzend gegevens</button>
+    </form>
+</div>
