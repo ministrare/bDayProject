63b6e58f26a37114165cf7a68c813ad47773d131
Lesley Forn <ministrare@hotmail.com>
2019-02-09 14:30:37 +0100

    corrections

--- www/tinymvc/myapp/views/index_view.php
+++ www/tinymvc/myapp/views/index_view.php
@@ -4,8 +4,8 @@
             <h2 class="h3 mb-3 font-weight-normal">Vul hier je gegevens in</h2>
             <form action="" method="post">
                 <div class="form-group">
-
-                    <input type="email" name="email" class="form-control" placeholder="Email adres">
+                    <label for="inputEmail">Email address</label>
+                    <input id="inputEmail" type="email" name="email" class="form-control" placeholder="Email adres">
                 </div>
                 <input type="text" name="surname" class="form-control" placeholder="Voornaam">
                 <input type="text" name="lastname" class="form-control" placeholder="Achternaam">
